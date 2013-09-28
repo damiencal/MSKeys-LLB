dba35bf570913f7a2cd23a4a38c95bc02fa7dfc5
Damien <damiencal@192.168.1.59>
2013-09-28 21:08:17 +0200

    wr

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
     <meta name="description" content="MSDN LLB Keys">
     <meta name="author" content="LLB">
     <link rel="shortcut icon" href="favicon.png">
-
     <title>MSKeys LLB</title>
-
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
   </head>
@@ -16,34 +14,12 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">MSKeys LLB</a>
+          <a class="navbar-brand">MSKeys LLB</a>
         </div>
         <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
           <form class="navbar-form navbar-right">
             <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
+              <input type="text" placeholder="Login" class="form-control">
             </div>
             <div class="form-group">
               <input type="password" placeholder="Password" class="form-control">
@@ -53,17 +29,13 @@
         </div><!--/.navbar-collapse -->
       </div>
     </div>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg">Learn more »</a></p>
-      </div>
-    </div>
     <div class="container">
       <hr>
+      <script language="javascript">
+if(window.clipboardData)document.write('<font size=2><i>Pour copier le code dans le Presse-Papier, double-cliquez dessus !</i></font>');
+</SCRIPT>
+<pre style="background-color:#336699;color=#FFFFFF;" ondblclick='javascript:window.clipboardData.setData("Text", this.innerText);alert("Le texte est dans le Presse-Papier.\nVous pouvez maintenant le coller où vous le souhaitez.");'>
+</pre>
       <footer>
         <p>© LLB 2013</p>
       </footer>
