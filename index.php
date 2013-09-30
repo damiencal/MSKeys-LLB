5b09e623d8c5141e37462f2bb8645ca7dfe3b5f6
BtsSio2 <BtsSio2@10.221.92.18>
2013-09-30 17:32:03 +0200

    Update total normally

--- index.php
+++ index.php
@@ -29,27 +29,10 @@
         </div><!--/.navbar-collapse -->
       </div>
     </div>
-<<<<<<< HEAD
-=======
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <script language="javascript">
-        if(window.clipboardData)document.write('<font size=2><i>Pour copier le code dans le Presse-Papier, double-cliquez dessus !</i></font>');
-        </script>
-        <pre style="background-color:#336699;color=#FFFFFF;" ondblclick='javascript:window.clipboardData.setData("Text", this.innerText);alert("Le texte est dans le Presse-Papier.\nVous pouvez maintenant le coller où vous le souhaitez.");'>
-        </pre>
-      </div>
-    </div>
->>>>>>> branch 'master' of https://github.com/damiencal/MSKeys-LLB.git
-    <div class="container">
-      <hr>
-      <script language="javascript">
-if(window.clipboardData)document.write('<font size=2><i>Pour copier le code dans le Presse-Papier, double-cliquez dessus !</i></font>');
-</SCRIPT>
-<pre style="background-color:#336699;color=#FFFFFF;" ondblclick='javascript:window.clipboardData.setData("Text", this.innerText);alert("Le texte est dans le Presse-Papier.\nVous pouvez maintenant le coller où vous le souhaitez.");'>
-</pre>
       <footer>
         <p>© LLB 2013</p>
       </footer>
