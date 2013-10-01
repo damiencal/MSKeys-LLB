3bf2672ebf885b831a3c029dc2d90011f66cc050
Metasabaca <metasabaca@gmail.com>
2013-10-01 17:11:57 +0200

    ssd

--- fonctionDB.php
+++ fonctionDB.php
@@ -1,4 +1,6 @@
 <?php
+
+// To connect to the database
 function connect(){
     $hote = "10.22.50.178";
     $utilisateur = "mskeys";
@@ -6,26 +8,75 @@ function connect(){
   
     $connexion=mysql_connect($hote, $utilisateur, $motdepasse);
     $nombdd="mskeys";
-    mysql_select_db($nombdd, $connexion);
+    mysql_select_db($nombdd, $connexion) or die('Connection error');
 }
 
-function select($select){
+
+// To select the key
+function select_key($select){
+    
+    $select = "SELECT key FROM mskeys WHERE key='$key';";
+    
     mysql_query($select,$connexion);
     return $select;
 }
+
+
+// To select the OS
+function select_category($select){
     
-function ajout($ajout){
+    $select = "SELECT key FROM mskeys WHERE category='$category';";
+    
+    mysql_query($select,$connexion);
+    return $select;
+}
+
+
+// To add a new key
+function add_key($ajout){
+    
+    $ajout = "INSERT INTO mskeys (category,key,used) VALUES ('$category','$key',$used);";
+    
     mysql_query($ajout,$connexion);
     return $ajout;
 }
 
-function suppr($suppr){
+
+// To delete a key
+function delete_key($suppr){
+    
+    $suppr = "DELETE FROM mskeys WHERE key='$key';";
+    
     mysql_query($suppr,$connexion);
     return $suppr;
 }
 
-function update($update){
-     $update = "UPDATE";
+
+// To delete all rows
+function delete_all($suppr){
+    
+    $suppr = "DELETE * FROM mskeys;";
+    
+    mysql_query($suppr,$connexion);
+    return $suppr;
+}
+
+
+// To update an already existing key that has been used
+function update_used($update){
+    
+     $update = "UPDATE mskeys SET used=0 WHERE key='$key';";
+     
+     mysql_query($update,$connexion);
+     return $update;
+}
+
+
+// To update an already existing category
+function update_category($update){
+    
+     $update = "UPDATE mskeys SET category='$new_category' WHERE category='$category';";
+     
      mysql_query($update,$connexion);
      return $update;
 }
