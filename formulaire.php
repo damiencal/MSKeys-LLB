03b013e0d0539667d969853769112a05f43c1346
loic <loic@loic-laptop>
2013-11-20 16:05:55 +0100

    Final

--- formulaire.php
+++ formulaire.php
@@ -38,7 +38,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand">MSKeys LLB</a>
+            <a class="navbar-brand" href="index.php">MSKeys LLB</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
